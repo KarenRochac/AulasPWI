539bcfffa71567ff3c82c3f67d51a7f4e87c6b9e
Karen Cristina Rocha <renkacharo@gmail.com>
2021-02-19 17:00:20 -0300

    receita

---
+++ pwii-19-02-2021/js/script.js
@@ -0,0 +1,8 @@
+ $('#enviarAlert').click(function() {
+    alert(
+        "Nome da Receita: " + $('#nome-receita').val() +
+        "\nNome da Receita: " + $('#ingrediente').val() +
+        "\nModo de Preparo: " + $('#preparo').val() +
+        '');
+    });
+ 
